34f7597670a51e092f3f4166a30fd15cec2167b8
gctziavas <gc.tziavas@gmail.com>
2022-02-28 10:50:47 +0200

    added IPs as global vars

--- src/main/java/io/nwdaf/analytics/response/builders/NfLoadLevelResponseBuilder.java
+++ src/main/java/io/nwdaf/analytics/response/builders/NfLoadLevelResponseBuilder.java
@@ -17,12 +17,16 @@ public class NfLoadLevelResponseBuilder {
 	public List <NfLoadLevelInformation> nfLoadLevelInformation(EventFilter eventFilter, TargetUeInformation tgtUe) {
 		List <NfLoadLevelInformation> nfLoadLevelInformation = new ArrayList<NfLoadLevelInformation>();
 		
-		Boolean anySupi = tgtUe.isAnyUe();
-		if(anySupi) {
+		Boolean anyUe = tgtUe.isAnyUe();
+		if(anyUe) {
+			//Find all the UEs of the given area
+			
 			
 		}
 		else {
 			List<String> supis = tgtUe.getSupis();
+			
+			
 		}
 		
 		return nfLoadLevelInformation;
